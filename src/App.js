f0970a1376ce67d432f7a6fb4a44fa94093606ef
jinpark <jinpark0625@gmail.com>
2021-08-15 02:30:21 +0900

    add login

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Login from '../src/component/login/login'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
